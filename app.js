220a12939d7950761c6eade7753d96fb0047959d
MarianaRTeixiera <marianamrochateixeira@gmail.com>
2020-12-17 14:54:15 +0000

    create app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+// Add needed variables
+const querty = document.getElementById('querty');
+const phrase = document.getElementById('phrase');
+const btnReset = document.getElementsByClassName('btn__reset');
+var missed = 0;
+
+//Create an array named phrases
+const namedPhrases = [
+    'Iron Man',
+    'Captain America The First Avenger',
+    'Guardians of the Galaxy',
+    'The Incredible Hulk',
+    'Avengers EndGame'
+]
+
+//Attach an event listener to the “Start Game” button to hide the start
+
+//Create a getRandomPhraseAsArray function.
+
+//Create a checkLetter function
+
+//Add an event listener to the keyboard
+
+//Create a checkWin function
+

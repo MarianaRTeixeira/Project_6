12671d0dddbdce97049f8a9ef91a0f6ce915b9ea
MarianaRTeixiera <marianamrochateixeira@gmail.com>
2020-12-18 14:36:27 +0000

    first

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 // Add needed variables
 const querty = document.getElementById('querty');
 const phrase = document.getElementById('phrase');
-const btnReset = document.getElementsByClassName('btn__reset');
-var missed = 0;
+const startBtn = document.querySelector('a.btn__reset');
+const overlay = document.getElementById('overlay');
+
+
+
+
+//Add some extra text in the first page
+const startPhrase = document.getElementById('overlay');
+const startText = document.createTextNode('Are you ready?');
+startPhrase.appendChild(startText);
+
+//Attach an event listener to the “Start Game” button to hide the start
+startBtn.addEventListener('click', () => {
+    overlay.style.display = 'none';
+});
+
 
 //Create an array named phrases
 const namedPhrases = [
@@ -10,12 +24,34 @@ const namedPhrases = [
     'Captain America The First Avenger',
     'Guardians of the Galaxy',
     'The Incredible Hulk',
-    'Avengers EndGame'
+    'Avengers EndGame',
+    'Doctor Stange',
+    'Black Panther',
+    'SpiderMan',
+    'Black Widow',
+    'Captain Marvel'
 ]
-
-//Attach an event listener to the “Start Game” button to hide the start
+ 
+// create an list 
+function list (text) {
+    const li = document.createElement('li');
+    li.textContent = text;
+    URL.appendChild(li);
+}
 
 //Create a getRandomPhraseAsArray function.
+
+/*function game (){
+const gamePhrase = Math.floor((Math.random() * namedPhrases.length));
+document.getElementById('phrase').innerHTML = namedPhrases[i];*/
+
+
+
+
+
+
+
+
 
 //Create a checkLetter function
 

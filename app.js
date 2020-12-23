fce96b395259b6c9b14e211df145ec81e683f849
MarianaRTeixiera <marianamrochateixeira@gmail.com>
2020-12-23 13:59:42 +0000

    show letter working

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // Add needed variables
-const querty = document.getElementById('querty');
+const querty = document.querySelector('#querty');
 const phrase = document.getElementById('phrase');
 const ul = phrase.querySelector('ul');
 const startBtn = document.querySelector('a.btn__reset');
 const overlay = document.getElementById('overlay');
-
+const button = document.querySelector('button');
+const hearts = document.querySelectorAll('.tries img');
+let missed = 0;
 let score = 0
 
 
@@ -45,43 +47,56 @@ function addPhraseToDispaly(arr){
         list.textContent = arr[i];
         ul.appendChild(list);
             if( arr[i] === ' '){
-            list.className = 'space';
+                list.className = 'space';
             } else {
-            list.className = 'letter';
+                list.className = 'letter';
+                // Debug
+                //console.log(list);
             }
     }
 };
-let displayPhrase = getRandomPhraseAsArray(arrPhrases);
+
 addPhraseToDispaly(arrPhrases[nPhrase]);
 
 // //check if a Letter is the place
 function checkLetter(btn) {
-    let check = document.getElementsByClassName('.letter');
+    let check = document.getElementsByClassName('letter');
     let match = null;
 
     for (i = 0; i < check.length; i++) {
-          if(check[i].textContent,toLowerCase() === btn.textContent) {
-              check[i].className = 'show';
-              match = check[i].textContent;
-          } 
+        // if(btn.textContent.toLowerCase() === check[i].textContent.toLowerCase()) {
+        if(btn.textContent.toLowerCase() === check[i].textContent.toLowerCase()) {
+            check[i].className = 'show';
+            //match = check[i].textContent.toLowerCase();
+            // Debug
+            //console.log(match);
+        } 
     }
-    return match;
-console.log(btn)
+    return 'ok'; //match;  
 }
-/* Sample
-let names = 'Iron Man';
-let letter = 'm';
 
-for (let i = 0; i < names.length; i++) {
-    const ln = names[i];
-    if (ln.toLowerCase() == letter){
-        console.log(ln);
+// //listen for the onscreen Keyboard to be clicked
+qwerty.addEventListener('click', (e) => {
+    if(e.target.tagName == 'BUTTON'){
+        e.target.className = 'chosen';
+        e.target.disable = true;
+    } else  { 
+        alert('Please choose a letter');
     }
-} */
+    let btnCheck = checkLetter(e.target);
+    if (btnCheck === 'null'){
+        
+     
+        missed += 1;
+        hearts[missed].src ="images/lostHeart.png";
+    }
+   // checkWin();  
+}); 
 
 
 // //check if the game has been won or lost
-// const checkWin = () => {};
+
+//function  checkWin = () => {};
 
 // //listen for the start game button to be pressed
 // startButton.addEventListener('click', () => { 
